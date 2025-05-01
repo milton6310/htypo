e6634480fab4ab18878668621a463d42be13b9ad
Milton Kim <milton6310@icloud.com>
2025-05-01 09:03:05 +0900

    Change background color

--- src/htypo_frontend/src/main.jsx
+++ src/htypo_frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthClient } from "@dfinity/auth-client";
-import App from './App';
+import App from './components/App';
 import "./index.scss";
 
 const init = async () => {
